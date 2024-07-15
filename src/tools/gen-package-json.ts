61767c2980663b9ee130fdb8acf511c51024a249
rca <nem@nem.x0.to>
2024-07-16 03:15:18 +0900

    ユーティリティ追加

---
+++ src/tools/gen-package-json.ts
@@ -0,0 +1,17 @@
+import {
+    name,
+    description,
+    version,
+    main,
+    license,
+    dependencies,
+} from 'pjson';
+const packageJson = {
+    name,
+    description,
+    version,
+    main,
+    license,
+    dependencies,
+};
+console.log(JSON.stringify(packageJson));
