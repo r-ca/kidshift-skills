ca72216a0d42ba7dbba10d1128896e292bbd7442
rca <nem@nem.x0.to>
2024-07-17 08:56:22 +0900

    attributeUtils作成 WIP

---
+++ src/attributeUtil.ts
@@ -0,0 +1,30 @@
+import * as Alexa from 'ask-sdk-core';
+
+class AttributeUtil {
+    private handlerInput: Alexa.HandlerInput;
+
+    constructor(handlerInput: Alexa.HandlerInput) {
+        this.handlerInput = handlerInput;
+    }
+
+    public async saveAttributes(): Promise<void> {
+        await this.handlerInput.attributesManager.savePersistentAttributes();
+    }
+
+    public async setToken(token: string): Promise<void> {
+        return await this.handlerInput.attributesManager.getPersistentAttributes().then((attributes) => {
+            attributes.token = token;
+            return attributes;
+        }).then((attributes) => {
+            this.handlerInput.attributesManager.setPersistentAttributes(attributes);
+        }).finally(() => {
+            return this.saveAttributes();
+        });
+    }
+
+    public async getToken(): Promise<string> {
+        return await this.handlerInput.attributesManager.getPersistentAttributes().then((attributes) => {
+            return attributes.token;
+        });
+    }
+}
