ca640953a8ce1457b54dd0ef11599d507f4bdd03
rca <nem@nem.x0.to>
2024-07-17 08:19:12 +0900

    LoginWithCodeRequest作成

--- src/models/Common.ts
+++ src/models/Common.ts
@@ -8,7 +8,12 @@ interface TokenResponse extends TokenBaseItem {
     // 追加プロパティ
 }
 
+interface LoginWithCodeRequest {
+    code: string,
+}
+
 export {
     TokenBaseItem,
-    TokenResponse
+    TokenResponse,
+    LoginWithCodeRequest
 }
