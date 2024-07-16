982e92e693b3586a50ee6b53fda8db2e819a9992
rca <nem@nem.x0.to>
2024-07-17 07:25:07 +0900

    service修正

--- src/service/MetaService.ts
+++ src/service/MetaService.ts
@@ -1,9 +1,13 @@
 import api from '../api'
 
 class MetaService {
-  async getMeta() {
-    return await api.get<any>('/meta');
-  }
+    async getMeta() {
+        return await api.get<any>('/meta');
+    }
+
+    async getPing() {
+        return await api.get<any>('/meta/ping');
+    }
 }
 
 export default new MetaService();
