ecef7658122bce3f2e2a181a2af5a4be67698b95
ろむねこ <nem@nem.x0.to>
2024-07-16 11:49:56 +0900

    withShouldEndSession(true)

--- src/index.ts
+++ src/index.ts
@@ -76,6 +76,7 @@ const KidShiftAuthIntentHandler = {
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
+            .withShouldEndSession(true)
             .getResponse();
     }
 };
