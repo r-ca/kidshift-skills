83d2853e4c710c3b3f433fee7992a1dfda7e9e0d
ろむねこ <nem@nem.x0.to>
2024-07-17 13:51:23 +0900

    serviceBase

--- src/service/ChildService.ts
+++ src/service/ChildService.ts
@@ -1,7 +1,8 @@
 import api from '../api';
 import { ChildListResponse } from '../models/Child';
+import _ServiceBase from './_ServiceBase';
 
-class ChildService {
+class ChildService extends _ServiceBase {
   async getChildList() {
     return api.get<ChildListResponse>('/child');
   }
