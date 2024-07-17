faa2249c1086820e494169a37cc95ae202c2494d
rca <nem@nem.x0.to>
2024-07-17 09:12:46 +0900

    ServiceBaseを継承するように

--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -1,7 +1,8 @@
 import api from '../api'
 import { TokenResponse } from '../models/Common';
+import _ServiceBase from './_ServiceBase';
 
-class AuthService {
+class AuthService extends _ServiceBase {
     async login(loginCode: string) {
         return await api.post<TokenResponse>('/parent/auth/login' , { code: loginCode });
     }
