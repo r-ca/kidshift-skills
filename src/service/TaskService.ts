0794b706e4b18236a7796c6e31082c7ffb8652ee
rca <nem@nem.x0.to>
2024-07-17 09:02:03 +0900

    getTasks実装

--- src/service/TaskService.ts
+++ src/service/TaskService.ts
@@ -1,7 +1,10 @@
 import api from '../api';
+import { TaskListResponse } from '../models/Task';
 
 class TaskService {
-
+    async getTasks() {
+        return await api.get<TaskListResponse>('/task');
+    }
 }
 
 export default TaskService;
