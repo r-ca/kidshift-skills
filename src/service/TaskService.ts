f823a5961154dd7d126fc1042775819ce3f6dbf5
rca <nem@nem.x0.to>
2024-07-17 09:41:00 +0900

    completeTask作成

--- src/service/TaskService.ts
+++ src/service/TaskService.ts
@@ -6,6 +6,10 @@ class TaskService extends _ServiceBase {
     async getTasks() {
         return await api.get<TaskListResponse>('/task');
     }
+
+    async completeTask(taskId: string) {
+        return await api.post<void>(`/task/${taskId}/complete`, {})
+    }
 }
 
 export default new TaskService();
