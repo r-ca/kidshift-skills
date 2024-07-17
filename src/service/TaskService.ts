cc0692853f2986ab14817edc1c39bdee919027b4
rca <nem@nem.x0.to>
2024-07-17 09:12:09 +0900

    ServiceBaseを継承するように

--- src/service/TaskService.ts
+++ src/service/TaskService.ts
@@ -1,7 +1,8 @@
 import api from '../api';
 import { TaskListResponse } from '../models/Task';
+import _ServiceBase from './_ServiceBase';
 
-class TaskService {
+class TaskService extends _ServiceBase {
     async getTasks() {
         return await api.get<TaskListResponse>('/task');
     }
