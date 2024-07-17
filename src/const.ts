3629140c9f760b42175509fa9612a10fdb431ad2
ろむねこ <nem@nem.x0.to>
2024-07-17 16:12:10 +0900

    messages作成

---
+++ src/const.ts
@@ -0,0 +1,11 @@
+const MESSAGES = Object.freeze({
+    TASK_COMPLETED: 'タスクの完了を記録しました',
+    TASK_NOT_FOUND: 'タスクが見つかりません',
+    CHILD_NOT_FOUND: '子供が見つかりません',
+    ERROR_OCCURRED: 'エラーが発生しました',
+    NOT_LOGGED_IN: 'ログインしていません',
+    LOGGED_IN: 'ログインしました',
+});
+
+
+export { MESSAGES };
